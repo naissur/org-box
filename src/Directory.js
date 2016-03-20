e2151c66851d57b92f33e207dfc7f8e2aad9a049
Vlad Aituganov <vlad.aituganov@gmail.com>
2016-03-21 06:36:55 +0800

    Going through directories

--- src/Directory.js
+++ src/Directory.js
@@ -5,6 +5,8 @@ import { bindActionCreators } from 'redux'
 import { authorizeUser, userGetInfo } from './actions/user';
 import { folderGet } from './actions/folder';
 
+const path = require('path');
+
 
 /* eslint camelcase: 0 */
 const renderEntry = ({ entry, onNavigate }) => {
@@ -27,6 +29,7 @@ const renderEntry = ({ entry, onNavigate }) => {
 
 @connect( state => ({
   user: state.user,
+  folderPath: state.folder.folderPath,
   folder: state.folder.folder,
   folderLoaded: state.folder.folderLoaded,
   folderLoading: state.folder.folderLoading
@@ -46,7 +49,43 @@ export default class Directory extends Component {
   }
 
   onNavigateEntry(path) {
-    console.log('navigate', path);
+    this.props.folderGet(path);
+  }
+
+  getUpPath() {
+    const { folderPath } = this.props;
+
+    const upPath = path.resolve(folderPath, '../');
+
+    if (upPath === '/') return '';
+    return upPath;
+  }
+
+  renderUp() {
+    const { folderPath } = this.props;
+
+    if (folderPath === '') return null;
+
+    const upPath = this.getUpPath();
+
+    return (
+      <a href='#' onClick={
+        ev => {
+          ev.preventDefault();
+          this.onNavigateEntry(upPath);
+        }}
+      >
+        Up
+      </a>
+    );
+  }
+
+  getHeader() {
+    return (
+      <h3>
+        Directory { this.renderUp() }
+      </h3>
+    );
   }
 
   render() {
@@ -59,6 +98,7 @@ export default class Directory extends Component {
     if (!folderLoaded && folderLoading) {
       return (
         <div>
+          { this.getHeader() }
           Loading folder...
         </div>
       );
@@ -74,10 +114,7 @@ export default class Directory extends Component {
 
     return (
       <div>
-        <h3>
-          Directory
-        </h3>
-
+        { this.getHeader() }
         <div>
           {entries.map(entry => renderEntry({ entry, onNavigate : ::this.onNavigateEntry }))}
         </div>
